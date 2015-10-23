cb16864f9b1fb9231b0cbb34022d0b32299d0205
marcoski <mtrognon@gmail.com>
2015-10-23 11:14:35 +0200

    setting composer package new name as commonhelp/lib

---
+++ src/Rss/tests/RssGrabberTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Commonhelp\Rss;
+
+use Commonhelp\Rss\Scraper\Scraper;
+
+class RssGrabberTest extends \PHPUnit_Framework_TestCase{
+	
+	private $url = 'http://www.dinamopress.it/inchieste/poligrow-il-volto-oscuro-del-capitalismo-italiano-in-colombia';
+	
+	public function testGrab(){
+		$grabber = new Scraper();
+		$grabber->setUrl($this->url);
+		$grabber->execute();
+		
+		print_r($grabber->getRelevantContent());
+	}
+	
+}
